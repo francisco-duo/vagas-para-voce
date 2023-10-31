ddeeb0ad5f34b3d91cd7ba6c0d40800553734856
francisco-duo <francisco.duo2000@gmail.com>
2023-10-31 19:40:13 -0300

    to louco

--- apps/vaga_para_voce/urls.py
+++ apps/vaga_para_voce/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     path('usuario/', views.users_view.user_dashboard, name='user_page'),
     path('cadastrar_vaga/', views.users_view.criar_vagas, name='cadastra_vaga'),
     path('vagas/', views.users_view.buscar_vagas, name='buscar_vaga'),
-    # path('vaga/<id: int>', views.users_view.visualizar_vaga, name='visualizar_vaga'),
 ]
