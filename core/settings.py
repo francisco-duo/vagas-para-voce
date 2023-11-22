0d9ef9444798569d2380f4532d98751da99e64ca
francisco-duo <francisco.duo2000@gmail.com>
2023-11-22 19:59:16 -0300

    database sqlite

--- core/settings.py
+++ core/settings.py
@@ -81,12 +81,8 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "database-1",
-        "USER": "postgres",
-        "PASSWORD": "admins3t4p3",
-        "HOST": "database-1.chi6ogtygwkh.us-east-1.rds.amazonaws.com",
-        "PORT": "5432",
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
