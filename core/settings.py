dc6fc89f1bd7724fe68d139c864460ad3a6fa491
francisco-duo <francisco.duo2000@gmail.com>
2023-11-22 19:51:45 -0300

    vlibras

--- core/settings.py
+++ core/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "database-1",
+        "USER": "postgres",
+        "PASSWORD": "admins3t4p3",
+        "HOST": "database-1.chi6ogtygwkh.us-east-1.rds.amazonaws.com",
+        "PORT": "5432",
     }
 }
 
