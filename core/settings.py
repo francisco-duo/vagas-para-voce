1e3199875d750d391ed2c762390997b24b6885bf
francisco-duo <francisco.duo2000@gmail.com>
2023-12-04 15:16:20 -0300

    collect static tentativa 1

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ PROJECT_ROOT = os.path.dirname(__file__)
 SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "*",
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/static/'
 STATICFILE_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
